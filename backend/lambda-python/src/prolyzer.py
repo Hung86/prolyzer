9d320f51cd0102bab270d7b25ed4104954e4b3ed
David Billa <d.billa316@gmail.com>
2020-03-03 21:04:05 +0800

    Testing Tweepy Python Library

--- backend/lambda-python/src/prolyzer.py
+++ backend/lambda-python/src/prolyzer.py
@@ -1,21 +1,42 @@
 import json
 from datetime import datetime
 import numpy as np
+import os
+import tweepy as twp
 
 
 def prolyzer(event, context):
     # Playing around with numpy
-    a = np.arange(15).reshape(3, 5)
+	a = np.arange(15).reshape(3, 5)
+	consumer_key = 'k7bmKlFjUf3eyFcwpqi1D34aZ'
+	consumer_secret = 'l4u0IRz5AYt9HXThR5lWTN41dTX5UMrs3VmZnmNPZky1mJmf7M'
+	access_token = '1263822842-QDqzja2ZGdE2RWlL12UxEnMCcGsDUxDkxsfdDZc'
+	access_token_secret = 'gcRxVFfwpeQr8mGF4BLP9ScK2qJnxWZxnneoq0fQFDznq'
 
-    response = {
-        "statusCode": 200,
-        "body": json.dumps({
-            "message": "Welcome to Prolyzer on Python. The time is " + datetime.now().isoformat(),
-            "sampleNumpyArray": a.tolist()
-        })
-    }
+	auth = twp.OAuthHandler(consumer_key, consumer_secret)
+	auth.set_access_token(access_token, access_token_secret)
+	api = twp.API(auth)
 
-    return response
+	#tweets = api.user_timeline()
+	search_words = "#covid-19"
+	date_since = "2019-12-20"
+
+	totalTweets = ''
+	# Collect tweets
+	tweets = twp.Cursor(api.search,q=search_words,lang="en",since=date_since).items(10)
+	for tweet in tweets:
+		totalTweets = totalTweets + tweet.text
+
+	print(totalTweets)
+
+	response = {
+		"statusCode": 200,
+		"body": json.dumps({
+			"tweets": totalTweets
+		})
+	}
+
+	return response
 
 
 handler = prolyzer
