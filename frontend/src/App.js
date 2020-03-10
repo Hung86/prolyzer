7ac95cb325597e9f20db71812b40dda6192b6800
Alex Ang <angzhanyu@gmail.com>
2020-03-10 15:18:35 +0800

    Add frontend demo from meeting

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            randomNumber: 0
+        };
+    }
+
+    someMethod = () => {
+        this.setState({randomNumber: Math.random()});
+    };
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <p>
+                        {this.state.randomNumber}
+                    </p>
+                    <input
+                        placeholder="i am some text"
+
+                    />
+                    <button onClick={this.someMethod}>
+                        I am a button
+                    </button>
+                </header>
+            </div>
+        );
+    }
 }
 
 export default App;
