04376ea12d7da8ef79bfe2dc5124c2cada053300
David Billa <d.billa316@gmail.com>
2020-03-13 15:19:02 +0800

    Clean Tweet Text

--- backend/lambda-python/src/prolyzer.py
+++ backend/lambda-python/src/prolyzer.py
@@ -3,8 +3,11 @@ from datetime import datetime
 import os
 import tweepy as twp
 import json
+import re
 #import preprocessor as p
-
+def clean_tweet(tweet):
+    '''Utility function to clean tweet text by removing links, special characters using simple regex statements.''' 
+    return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t]) |(\w+:\/\/\S+)", " ", tweet).split())
 
 def prolyzer(event, context):
 	consumer_key = 'k7bmKlFjUf3eyFcwpqi1D34aZ'
@@ -26,7 +29,7 @@ def prolyzer(event, context):
 	#p.set_options(p.OPT.URL,p.OPT.MENTION,p.OPT.HASHTAG,p.OPT.RESERVED,p.OPT.NUMBER)
 	for tweet in tweets:
 		#totalTweets = totalTweets + p.clean(tweet.text)+ '\n'
-		totalTweets = totalTweets + tweet.text + '\n'
+		totalTweets = totalTweets + clean_text(tweet.text) + '\n'
 
 	print(totalTweets)
 
