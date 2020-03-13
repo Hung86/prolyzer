47bc407dbcc551f1d526b6e3ce07038656758a34
David Billa <d.billa316@gmail.com>
2020-03-13 16:28:23 +0800

    Add Tone Analysis

--- backend/lambda-python/src/prolyzer.py
+++ backend/lambda-python/src/prolyzer.py
@@ -3,6 +3,8 @@ from datetime import datetime
 import os
 import tweepy as twp
 import re
+from ibm_watson import ToneAnalyzerV3
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 #import preprocessor as p
 
 def prolyzer(event, context):
@@ -14,6 +16,16 @@ def prolyzer(event, context):
 	auth = twp.OAuthHandler(consumer_key, consumer_secret)
 	auth.set_access_token(access_token, access_token_secret)
 	api = twp.API(auth)
+    
+	authenticator = IAMAuthenticator('G7Um77PYTSWaI23-QJg-NqJrDrECeUnzn1_nSCbPkS-Q')
+	tone_analyzer = ToneAnalyzerV3(
+	version='2017-09-21',
+	authenticator=authenticator
+	)
+
+	tone_analyzer.set_service_url('https://api.us-south.tone-analyzer.watson.cloud.ibm.com/instances/046af756-8b3b-4b48-be22-627b30bf6321')
+
+	tone_analyzer.set_disable_ssl_verification(True)
 
 	#tweets = api.user_timeline()
 	search_words = "#covid-19"
@@ -25,14 +37,18 @@ def prolyzer(event, context):
 	#p.set_options(p.OPT.URL,p.OPT.MENTION,p.OPT.HASHTAG,p.OPT.RESERVED,p.OPT.NUMBER)
 	for tweet in tweets:
 		#totalTweets = totalTweets + p.clean(tweet.text)+ '\n'
-		totalTweets = totalTweets + ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t]) |(\w+:\/\/\S+)", " ", tweet.text).split()) + '\n'
+		totalTweets = totalTweets + ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t]) |(\w+:\/\/\S+)", " ", tweet.text).split())
 
 	print(totalTweets)
+	tone_analysis = tone_analyzer.tone(
+	{'text': totalTweets},
+	content_type='application/json'
+	).get_result()
 
 	response = {
 		"statusCode": 200,
 		"body": json.dumps({
-			"tweets": totalTweets
+			"tone_response": tone_analysis
 		})
 	}
 
