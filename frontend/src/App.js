e3460e21558cda8c44d04ac05dac7be9f0652580
Hung Tran <tkhung2004@gmail.com>
2020-03-16 22:54:20 +0800

    Added Search UI and Dashboard UI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,36 +1,17 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Dashboard from './components/Dashboard';
+import SearchUI from './components/SearchUI';
+
 import './App.css';
 
-class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            randomNumber: 0
-        };
-    }
-
-    someMethod = () => {
-        this.setState({randomNumber: Math.random()});
-    };
-
-    render() {
-        return (
-            <div className="App">
-                <header className="App-header">
-                    <p>
-                        {this.state.randomNumber}
-                    </p>
-                    <input
-                        placeholder="i am some text"
-
-                    />
-                    <button onClick={this.someMethod}>
-                        I am a button
-                    </button>
-                </header>
-            </div>
-        );
-    }
-}
-
-export default App;
+export default function App() {
+    return (
+      <BrowserRouter>
+        <Switch>
+            <Route path="/" exact component={SearchUI} /> {/*start default url with search UI*/}
+            <Route path="/dashboard" exact component={Dashboard} /> {/*URL dashboard ui*/}
+        </Switch>
+      </BrowserRouter>
+    );
+  }
---
+++ frontend/src/components/SearchUI.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import { Redirect, Link } from 'react-router-dom'
+
+
+class SearchUI extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        redirect: false,
+        relevant_product: ""
+      };
+      this.handleFormSubmit = this.handleFormSubmit.bind(this);
+
+    }
+  
+    myChangeHandler = (event) => {
+      this.setState({relevant_product: event.target.value});
+
+    }
+  
+    goSearch() {
+      alert("dadadasdasdasdas")
+      this.props.history.push('/dash/');
+    }
+
+    handleFormSubmit(e) {
+      e.preventDefault();
+      console.log("FORM SUBMIT!");
+      //this.props.history.push('/dashboard/');
+      this.setState({ redirect: true });
+      // const { redirect } = this.state;
+  
+    }
+  
+    render() {
+      const { redirect } = this.state;
+      if (redirect) {
+        console.log("---------------- redirect to Dashboard: " + redirect)
+        return <Redirect to='/dashboard'/>;
+      }
+      return (
+        <div className='App'>
+            <form>
+                <h1>Welme come to Prolyzer</h1>
+                <div>
+                <p>Enter your product: {this.state.relevant_product}</p>
+                <input
+                  type='text'
+                  class='Text_Left'
+                  onChange={this.myChangeHandler}
+                  placeholder="Relevant product..."
+                />
+          
+                <button class='button' type="submit" onClick={this.handleFormSubmit}>Search</button>
+                </div>
+            </form>
+        </div>
+      );
+    }
+  }
+  
+  
+  export default SearchUI;
