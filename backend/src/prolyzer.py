cb8cddcac88a41610c2a47adadf41d4c8bc30c72
David Billa <d.billa316@gmail.com>
2020-04-01 15:37:58 +0800

    changes to DB

--- backend/src/prolyzer.py
+++ backend/src/prolyzer.py
@@ -7,6 +7,7 @@ from ibm_watson import ToneAnalyzerV3
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 import mysql.connector
 import urllib.parse
+import time
 
 
 def prolyzer(event, context):
@@ -35,36 +36,72 @@ def prolyzer(event, context):
 
     print("Connecting to database...")
     mydb = mysql.connector.connect(
-        host=os.getenv('DB_ENDPOINT'),
+        host="prolyzer-backend-production.cr28byc2iyut.ap-southeast-1.rds.amazonaws.com",
         user="admin",
         passwd="password",
-        database=os.getenv('TABLE_NAME', 'prolyzer')
+        database="prolyzer"
     )
     print("Connected to database")
 
     mycursor = mydb.cursor()
+    
+    now = time.strftime('%Y-%m-%d %H:%M:%S')
 
     search_words = search_term
 
     total_tweets = ''
     # Collect tweets
     print("Collecting 100 tweets for search term " + search_term)
-    tweets = twp.Cursor(api.search, q=search_words, lang="en").items(100)
+    tweets = twp.Cursor(api.search, q=search_words, lang="en",tweet_mode='extended').items(100)
 
     for tweet in tweets:
         total_tweets = total_tweets + ' '.join(
             re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t]) |(\w+:\/\/\S+)", " ", tweet.text).split())
+        hashtags = tweet.entities['hashtags']
+        thashtag = ''
+        for hashtag in hashtags:   
+            thashtag = thashtag + hashtag['text'] + ","
+        if thashtag.endswith(","):
+            thashtag = thashtag[:-1]
+        sql = "INSERT INTO tweets (search_term,full_text,geo,userlocation,hashtags,created_at) VALUES (%s, %s, %s, %s, %s, %s)"
+        val = (search_words,tweet.full_text,tweet.geo,tweet.user.location,thashtag,now)
+        mycursor.execute(sql, val)
 
     print(total_tweets)
     tone_analysis = tone_analyzer.tone(
         {'text': total_tweets},
         content_type='application/json'
     ).get_result()
+    
+    document_tones = tone_analysis.get('document_tone')
+    
+    score1 = ""
+    toneid1 = ""
+    tonename1 = ""
+    score2 = ""
+    toneid2 = ""
+    tonename2 = ""
+    for value in document_tones.values():
+        score1 = value[0].get('score')
+        toneid1 = value[0].get('tone_id')
+        tonename1 = value[0].get('tone_name')
+        try:
+            score2 = value[1].get('score')
+            toneid2 = value[1].get('tone_id')
+            tonename2 = value[1].get('tone_name')
+        except IndexError:
+            score2 = 'null'
+            toneid2 = 'null'
+            tonename2 = 'null'
 
     print("Inserting into database")
-    sql = "INSERT INTO results (search_term,tweets,results) VALUES (%s, %s, %s)"
-    val = (search_words, str(total_tweets), str(tone_analysis))
-    mycursor.execute(sql, val)
+    sql1 = "INSERT INTO resultsdump (search_term,tweetsdump,resultsdump,created_at) VALUES (%s, %s, %s, %s)"
+    val1 = (search_words,str(totalTweets),str(tone_analysis),now)
+    mycursor.execute(sql1, val1)
+
+    sql2 = "INSERT INTO results (search_term,score1,toneid1,tonename1,score2,toneid2,tonename2,user,created_at) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)"
+    val2 = (search_words,score1,toneid1,tonename1,score2,toneid2,tonename2,"test",now)
+    mycursor.execute(sql2, val2)
 
     mydb.commit()
     mycursor.close()
